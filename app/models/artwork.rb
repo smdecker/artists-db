a69575c82e41dbc606c7a8b89ae5c03e0a455eb0
smdecker <sdeckerm@gmail.com>
2018-03-26 05:30:25 -0400

    added artwork model

---
+++ app/models/artwork.rb
@@ -0,0 +1,7 @@
+class Artwork < ActiveRecord::Base
+	has_many :artwork_materials
+	has_many :materials, :through => :artwork_materials
+	has_many :artwork_categories
+	has_many :categories, :through => :artwork_categories
+	belongs_to :user
+end
