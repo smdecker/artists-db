96b010ef83cd8c5a849a91c164f2543ca82b668b
S•M•D <sdeckerm@gmail.com>
2018-03-27 19:29:47 -0400

    Update environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -12,8 +12,7 @@ CarrierWave.configure do |config|
 config.fog_credentials = {
 :provider               => 'AWS',
 :region					=> 'us-east-2',
-:aws_access_key_id      => 'AKIAIIEPQZAMDDZDPH4A',
-:aws_secret_access_key  => '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',
+
 }
 config.fog_directory  = 'artists-db'
 config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}
