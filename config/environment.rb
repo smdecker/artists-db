bf75360a5ee30a752919c0a7cbc64bdf518e585e
smdecker <sdeckerm@gmail.com>
2018-03-27 19:22:18 -0400

    added carrierwave to environment

--- config/environment.rb
+++ config/environment.rb
@@ -5,7 +5,18 @@ Bundler.require(:default, ENV['SINATRA_ENV'])
 
 ActiveRecord::Base.establish_connection(
   :adapter => "sqlite3",
-  :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
+  :database => 'gallery.db'
 )
+
+CarrierWave.configure do |config|
+config.fog_credentials = {
+:provider               => 'AWS',
+:region					=> 'us-east-2',
+:aws_access_key_id      => 'AKIAIIEPQZAMDDZDPH4A',
+:aws_secret_access_key  => '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',
+}
+config.fog_directory  = 'artists-db'
+config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}
+end
 
 require_all 'app'
