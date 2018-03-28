e878060bdca169664959c737e42429f5f6bd8cd9
smdecker <sdeckerm@gmail.com>
2018-03-28 05:24:36 -0400

    add new artworks route

--- app/controllers/artwork_controller.rb
+++ app/controllers/artwork_controller.rb
@@ -8,4 +8,14 @@ class ArtworkController < ApplicationController
       erb :"artworks/index"
     end
   end 
+
+  get '/artworks/new' do
+		if !logged_in?
+			redirect to "/"
+		else
+      @user = current_user
+
+			erb :"artworks/new_artwork"
+		end
+	end
 end
