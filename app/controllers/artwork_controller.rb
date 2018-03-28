08cfcbf876cd7875bf64c495a3887dfadeb3071d
smdecker <sdeckerm@gmail.com>
2018-03-28 05:13:09 -0400

    artworks index

--- app/controllers/artwork_controller.rb
+++ app/controllers/artwork_controller.rb
@@ -1,2 +1,11 @@
 class ArtworkController < ApplicationController
+	get '/artworks' do
+    if !logged_in?
+      redirect to "/"
+    else
+      @user = current_user
+
+      erb :"artworks/index"
+    end
+  end 
 end
