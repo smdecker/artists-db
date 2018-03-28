4c2007880064abcd7a73429441af3dc5c24d2a76
smdecker <sdeckerm@gmail.com>
2018-03-28 03:09:26 -0400

    enabled sessions and created signup/login page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,10 +5,50 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+  	set :session_secret, "secret"
   end
 
-  get "/" do
-    erb :welcome
+  get '/' do
+		if !logged_in?
+			erb :login
+		else
+			erb :welcome
+		end
+	end
+
+	get '/signup' do
+    if !logged_in?
+		  erb :signup
+    else
+      redirect to "/"
+    end
+	end
+
+	post '/signup' do
+    if params[:username].empty? || params[:password].empty?
+
+      redirect to "/signup"
+    else
+      user = User.new(username: params[:username], password: params[:password])
+  		if user.save
+        session[:user_id] = user.id
+  			redirect to "/"
+  		else
+
+  			redirect to "/signup"
+  		end
+    end
+	end
+
+	helpers do
+    def logged_in?
+      !!current_user
+    end
+
+    def current_user
+      @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+    end
   end
 
 end
