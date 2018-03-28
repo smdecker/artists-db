4e632dbced0555969f4dd06dc8160f21e7285a77
smdecker <sdeckerm@gmail.com>
2018-03-28 03:31:31 -0400

    created login route

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,6 +17,16 @@ class ApplicationController < Sinatra::Base
 		end
 	end
 
+	post 'login' do
+		user = User.find_by(username: params[:username])
+		if user && user.authenticate(params[:password])
+			session[:user_id] = user.id
+			redirect to "/"
+		else
+			redirect to "/"
+		end
+	end
+
 	get '/signup' do
 		if !logged_in?
 			erb :signup
